b1cdd80edb068b55ae81f1577d77c4975fb81b02
Chris <cpoulter@hotmail.co.uk>
2020-02-26 08:52:48 +0000

    code clean up

--- src/utils/email.js
+++ src/utils/email.js
@@ -3,18 +3,19 @@ const { fetch } = require('./http');
 const { format, base64Encode } = require('./string');
 const config = require('./config');
 
+const mailgunUrl = `https://api.eu.mailgun.net/v3/${config.MAILGUN_DOMAIN}/messages`;
+const mailgunAuth = `Basic ${base64Encode(`api:${config.MAILGUN_PASSWORD}`)}`;
+
 module.exports.sendEmail = message => {
     const template = emailTemplates[message.template];
     const subject = format(template.subject, message.context);
     const html = format(template.html, message.context);
 
     return fetch({
-        url: `https://api.eu.mailgun.net/v3/${config.MAILGUN_DOMAIN}/messages`,
+        url: mailgunUrl,
         method: 'POST',
         headers: {
-            Authorization: `Basic ${base64Encode(
-                `api:${config.MAILGUN_PASSWORD}`
-            )}`
+            Authorization: mailgunAuth
         },
         body: {
             from: config.MAILGUN_NOREPLY,
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch');
 const { URLSearchParams } = require('url');
 
-module.exports.fetch = async config => {
+module.exports.fetch = async request => {
     let result = undefined;
 
     try {
-        const response = await fetch(config.url, {
-            ...config,
-            body: new URLSearchParams(config.body)
+        const response = await fetch(request.url, {
+            ...request,
+            body: new URLSearchParams(request.body)
         });
         result = await response.json();
     } catch (error) {
