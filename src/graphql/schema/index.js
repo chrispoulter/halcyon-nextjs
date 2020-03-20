338c80f42424f94e47539ff2967104d42efd4898
Chris <cpoulter@hotmail.co.uk>
2020-03-20 16:30:06 +0000

    schema refactor

--- src/graphql/schema/index.js
+++ src/graphql/schema/index.js
@@ -1,31 +1,11 @@
 const { gql } = require('apollo-server');
 
+const linkSchema = require('./linkSchema');
 const accountSchema = require('./accountSchema');
 const manageSchema = require('./manageSchema');
 const seedSchema = require('./seedSchema');
 const tokenSchema = require('./tokenSchema');
 const userSchema = require('./userSchema');
-
-const linkSchema = gql`
-    scalar Date
-
-    type MutationResponse {
-        message: String
-        code: String
-    }
-
-    type Query {
-        _: Boolean
-    }
-
-    type Mutation {
-        _: Boolean
-    }
-
-    type Subscription {
-        _: Boolean
-    }
-`;
 
 module.exports = [
     linkSchema,
