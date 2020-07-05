077945ac5f119ca03d8b5f4486eb03d42e387824
Chris <cpoulter@hotmail.co.uk>
2020-07-05 09:58:49 +0100

    code clean up

--- src/server.js
+++ src/server.js
@@ -1,20 +1,14 @@
 const { ApolloServer } = require('apollo-server');
-const { typeDefs, resolvers, context, subscriptions } = require('./graphql');
-const { loggerPlugin } = require('./utils/logger');
+const { typeDefs, resolvers, context } = require('./schema');
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context,
-    subscriptions,
-    plugins: [loggerPlugin],
     introspection: true,
     playground: true
 });
 
-server
-    .listen({ port: process.env.PORT || 3001 })
-    .then(({ url, subscriptionsUrl }) => {
-        console.log(`Server ready at ${url}`);
-        console.log(`Subscriptions ready at ${subscriptionsUrl}`);
-    });
+server.listen({ port: process.env.PORT || 3001 }).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
