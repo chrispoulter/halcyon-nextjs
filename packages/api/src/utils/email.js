2775a6309d8ad82ee3886fb60b160236159ed83c
Chris Poulter <cpoulter@hotmail.co.uk>
2021-02-06 13:55:51 +0000

    updated email templates

--- packages/api/src/utils/email.js
+++ packages/api/src/utils/email.js
@@ -1,4 +1,3 @@
-import { emailTemplates } from '../data/emailTemplates';
 import { format } from './string';
 import { fetch } from './http';
 import { base64Encode } from './encode';
@@ -6,6 +5,14 @@ import { config } from './config';
 
 const mailgunUrl = `https://api.mailgun.net/v3/${config.MAILGUN_DOMAIN}/messages`;
 const mailgunAuth = `Basic ${base64Encode(`api:${config.MAILGUN_APIKEY}`)}`;
+
+const emailTemplates = {
+    resetPassword: {
+        subject: 'Password Reset // Halcyon',
+        html:
+            '<html><head><title>Halcyon</title></head><body><p style="font-family: verdana, geneva, sans-serif; font-size: 11px;">We have received a request to reset your password.</p><p style="font-family: verdana, geneva, sans-serif; font-size: 11px;">In order to complete the process and select a new password please click here:</p><p style="font-family: verdana, geneva, sans-serif; font-size: 11px;"><a href="https://halcyon.chrispoulter.com/reset-password/{token}">Reset your password</a></p><p style="font-family: verdana, geneva, sans-serif; font-size: 11px;"><strong>Important</strong>: If you did not request a password reset do not worry. Your account is still secure and your old password will remain active.</p><p style="font-family: verdana, geneva, sans-serif; font-size: 11px;">Regards,<br /><strong>Halcyon</strong><br /><a href="https://halcyon.chrispoulter.com">https://halcyon.chrispoulter.com</a></p></body></html>'
+    }
+};
 
 export const sendEmail = message => {
     const template = emailTemplates[message.template];
