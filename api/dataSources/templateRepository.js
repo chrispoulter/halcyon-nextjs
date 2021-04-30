966a56c1fb72e147dacdd9c369ab3a7cda5865f4
Chris Poulter <cpoulter@hotmail.co.uk>
2021-04-30 16:06:19 +0100

    One table (#7)
    * use one dynamodb table
    
    * updated getAll
    
    * updated existing repos
    
    * inherit from base repo
    
    * code clean up

--- api/dataSources/templateRepository.js
+++ api/dataSources/templateRepository.js
@@ -1,51 +1,17 @@
-import AWS from 'aws-sdk';
-import { DataSource } from 'apollo-datasource';
-import { config } from '../utils/config';
+import { DynamoDBRepository } from './dynamoDBRepository';
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: config.REGION,
-    endpoint: config.STAGE === 'local' ? 'http://localhost:8000' : undefined
-});
+export class TemplateRepository extends DynamoDBRepository {
+    entity = 'TEMPLATE';
+    gs1Key = 'key';
 
-const tableName = `halcyon-${config.STAGE}-templates`;
+    searchFunc = template => template.key;
 
-export class TemplateRepository extends DataSource {
-    async getByKey(key) {
-        const params = {
-            TableName: tableName,
-            Key: { key }
-        };
-
-        const result = await dynamoDb.get(params).promise();
-
-        return this._map(result.Item);
-    }
-
-    async update(template) {
-        const params = {
-            TableName: tableName,
-            Item: template
-        };
-
-        await dynamoDb.put(params).promise();
-
-        return this._map(template);
-    }
-
-    async upsert(template) {
-        const existing = await this.getByKey(template.key);
-        return this.update({ ...existing, ...template });
-    }
-
-    _map = item => {
-        if (!item) {
-            return undefined;
+    sortOptions = {
+        DEFAULT: {
+            name: 'KEY_ASC',
+            valueFunc: template => template.key
         }
-
-        return {
-            key: item.key,
-            subject: item.subject,
-            html: item.html
-        };
     };
+
+    getByKey = super.getByGs1;
 }
--- api/dataSources/userRepository.js
+++ api/dataSources/userRepository.js
@@ -1,193 +1,32 @@
-import AWS from 'aws-sdk';
-import { DataSource } from 'apollo-datasource';
-import { v4 as uuidv4 } from 'uuid';
-import { config } from '../utils/config';
+import { DynamoDBRepository } from './dynamoDBRepository';
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: config.REGION,
-    endpoint: config.STAGE === 'local' ? 'http://localhost:8000' : undefined
-});
+export class UserRepository extends DynamoDBRepository {
+    entity = 'USER';
+    gs1Key = 'emailAddress';
 
-const tableName = `halcyon-${config.STAGE}-users`;
+    searchFunc = user =>
+        `${user.firstName} ${user.lastName} ${user.emailAddress}`;
 
-const indexes = {
-    EMAIL_ADDRESS: 'emailAddress-index'
-};
-
-const searchFunc = user =>
-    `${user.firstName} ${user.lastName} ${user.emailAddress}`;
-
-const sortOptions = {
-    NAME_ASC: {
-        name: 'NAME_ASC',
-        valueFunc: user => `${user.firstName} ${user.lastName}`
-    },
-    NAME_DESC: {
-        name: 'NAME_DESC',
-        valueFunc: user => `${user.firstName} ${user.lastName}`,
-        desc: true
-    },
-    EMAIL_ADDRESS_ASC: {
-        name: 'EMAIL_ADDRESS_ASC',
-        valueFunc: user => user.emailAddress
-    },
-    EMAIL_ADDRESS_DESC: {
-        name: 'EMAIL_ADDRESS_DESC',
-        valueFunc: user => user.emailAddress,
-        desc: true
-    }
-};
-
-export class UserRepository extends DataSource {
-    async getById(id) {
-        const params = {
-            TableName: tableName,
-            Key: { id }
-        };
-
-        const result = await dynamoDb.get(params).promise();
-
-        return this._map(result.Item);
-    }
-
-    async getByEmailAddress(emailAddress) {
-        const params = {
-            TableName: tableName,
-            IndexName: indexes.EMAIL_ADDRESS,
-            ExpressionAttributeNames: { '#emailAddress': 'emailAddress' },
-            ExpressionAttributeValues: { ':emailAddress': emailAddress },
-            KeyConditionExpression: '#emailAddress = :emailAddress',
-            Limit: 1
-        };
-
-        const result = await dynamoDb.query(params).promise();
-
-        return this._map(result.Items[0]);
-    }
-
-    create = this.update;
-
-    async update(user) {
-        const item = this._generate(user);
-
-        const params = {
-            TableName: tableName,
-            Item: item
-        };
-
-        await dynamoDb.put(params).promise();
-
-        return this._map(item);
-    }
-
-    async remove(user) {
-        const params = {
-            TableName: tableName,
-            Key: { id: user.id }
-        };
-
-        await dynamoDb.delete(params).promise();
-
-        return this._map(user);
-    }
-
-    async upsert(user) {
-        const existing = await this.getByEmailAddress(user.emailAddress);
-        return this.update({ ...existing, ...user });
-    }
-
-    async search(request) {
-        const users = this._search(
-            await this._getAll(),
-            searchFunc,
-            request.search
-        );
-
-        this._sort(users, sortOptions[request.sort] || sortOptions.NAME_ASC);
-
-        return this._paginate(users, request.page || 1, request.size || 10);
-    }
-
-    _getAll = async () => {
-        const params = {
-            TableName: tableName
-        };
-
-        let items = [];
-        let result;
-
-        do {
-            result = await dynamoDb.scan(params).promise();
-            result.Items.forEach(item => items.push(item));
-            params.ExclusiveStartKey = result.LastEvaluatedKey;
-        } while (result.LastEvaluatedKey !== undefined);
-
-        return items;
-    };
-
-    _search = (items, valueFunc, filter) => {
-        if (!filter) {
-            return items;
+    sortOptions = {
+        DEFAULT: {
+            name: 'NAME_ASC',
+            valueFunc: user => `${user.firstName} ${user.lastName}`
+        },
+        NAME_DESC: {
+            name: 'NAME_DESC',
+            valueFunc: user => `${user.firstName} ${user.lastName}`,
+            desc: true
+        },
+        EMAIL_ADDRESS_ASC: {
+            name: 'EMAIL_ADDRESS_ASC',
+            valueFunc: user => user.emailAddress
+        },
+        EMAIL_ADDRESS_DESC: {
+            name: 'EMAIL_ADDRESS_DESC',
+            valueFunc: user => user.emailAddress,
+            desc: true
         }
-
-        const lowerFilter = filter.toLowerCase();
-
-        return items.filter(item =>
-            valueFunc(item).toLowerCase().includes(lowerFilter)
-        );
     };
 
-    _sort = (items, { valueFunc, desc }) => {
-        items.sort((a, b) =>
-            valueFunc(a).toLowerCase() > valueFunc(b).toLowerCase()
-                ? desc
-                    ? -1
-                    : 1
-                : valueFunc(b).toLowerCase() > valueFunc(a).toLowerCase()
-                ? desc
-                    ? 1
-                    : -1
-                : 0
-        );
-    };
-
-    _paginate = (items, page, size) => {
-        const pageCount = (items.length + size - 1) / size;
-
-        if (page > 1) {
-            items = items.filter((_, i) => i >= (page - 1) * size);
-        }
-
-        return {
-            items: items.filter((_, i) => i < size).map(this._map),
-            hasNextPage: page < pageCount,
-            hasPreviousPage: page > 1
-        };
-    };
-
-    _generate = item => ({
-        ...item,
-        id: item.id || uuidv4(),
-        isLockedOut: item.isLockedOut,
-        password: item.password,
-        passwordResetToken: item.passwordResetToken
-    });
-
-    _map = item => {
-        if (!item) {
-            return undefined;
-        }
-
-        return {
-            id: item.id,
-            emailAddress: item.emailAddress,
-            password: item.password,
-            firstName: item.firstName,
-            lastName: item.lastName,
-            dateOfBirth: item.dateOfBirth,
-            isLockedOut: item.isLockedOut,
-            passwordResetToken: item.passwordResetToken,
-            roles: item.roles
-        };
-    };
+    getByEmailAddress = super.getByGs1;
 }
