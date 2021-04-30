ab1cff717bfac4e87fbec7ec53efdb26aa1d9963
Chris Poulter <cpoulter@hotmail.co.uk>
2021-04-30 14:50:46 +0100

    code clean up

--- api/dataSources/index.js
+++ api/dataSources/index.js
@@ -1,9 +1,7 @@
 import { UserRepository } from './userRepository';
 import { TemplateRepository } from './templateRepository';
-import { DynamoDBRepository } from './dynamoDBRepository';
 
 export const dataSources = () => ({
     users: new UserRepository(),
-    templates: new TemplateRepository(),
-    dynamo: new DynamoDBRepository()
+    templates: new TemplateRepository()
 });
