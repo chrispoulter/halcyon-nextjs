fb77939289183393949b29e0a9f6c9d2b82b5b49
Chris Poulter <cpoulter@hotmail.co.uk>
2021-09-13 10:37:48 +0100

    route refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import {
     AuthProvider,
     ApolloProvider,
@@ -7,26 +7,11 @@ import {
     ToastProvider,
     Header,
     Footer,
-    PrivateRoute,
     Spinner,
     Meta,
     ErrorBoundary
 } from './components';
-import {
-    HomePage,
-    NotFoundPage,
-    LoginPage,
-    RegisterPage,
-    ForgotPasswordPage,
-    ResetPasswordPage,
-    MyAccountPage,
-    UpdateProfilePage,
-    ChangePasswordPage,
-    UserPage,
-    CreateUserPage,
-    UpdateUserPage
-} from './pages';
-import { USER_ADMINISTRATOR_ROLES } from './utils/auth';
+import { Routes } from './Routes';
 
 export const App = () => (
     <Suspense fallback={<Spinner />}>
@@ -38,67 +23,7 @@ export const App = () => (
                             <Meta />
                             <Header />
                             <ErrorBoundary>
-                                <Switch>
-                                    <Route
-                                        path="/"
-                                        component={HomePage}
-                                        exact
-                                    />
-                                    <Route
-                                        path="/register"
-                                        component={RegisterPage}
-                                        exact
-                                    />
-                                    <Route
-                                        path="/login"
-                                        component={LoginPage}
-                                        exact
-                                    />
-                                    <Route
-                                        path="/forgot-password"
-                                        component={ForgotPasswordPage}
-                                        exact
-                                    />
-                                    <Route
-                                        path="/reset-password/:token"
-                                        component={ResetPasswordPage}
-                                        exact
-                                    />
-                                    <PrivateRoute
-                                        path="/my-account"
-                                        component={MyAccountPage}
-                                        exact
-                                    />
-                                    <PrivateRoute
-                                        path="/update-profile"
-                                        component={UpdateProfilePage}
-                                        exact
-                                    />
-                                    <PrivateRoute
-                                        path="/change-password"
-                                        component={ChangePasswordPage}
-                                        exact
-                                    />
-                                    <PrivateRoute
-                                        path="/user"
-                                        requiredRoles={USER_ADMINISTRATOR_ROLES}
-                                        component={UserPage}
-                                        exact
-                                    />
-                                    <PrivateRoute
-                                        path="/user/create"
-                                        requiredRoles={USER_ADMINISTRATOR_ROLES}
-                                        component={CreateUserPage}
-                                        exact
-                                    />
-                                    <PrivateRoute
-                                        path="/user/:id"
-                                        requiredRoles={USER_ADMINISTRATOR_ROLES}
-                                        component={UpdateUserPage}
-                                        exact
-                                    />
-                                    <Route component={NotFoundPage} />
-                                </Switch>
+                                <Routes />
                             </ErrorBoundary>
                             <Footer />
                         </ApolloProvider>
