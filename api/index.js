c6c62b870573dc70ded8676d6d6bb93d7cd0afcf
Chris Poulter <cpoulter@hotmail.co.uk>
2021-12-13 13:48:37 +0000

    updated build script

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ import { logger } from './utils/logger';
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(morgan("tiny", { stream: logger.stream }));
+app.use(morgan('tiny', { stream: logger.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
