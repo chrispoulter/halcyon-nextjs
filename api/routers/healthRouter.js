0dec6aa8c66c957bd5888efcee46737d3c038e0c
Chris Poulter <cpoulter@hotmail.co.uk>
2021-12-15 10:53:15 +0000

    added version to health check

--- api/routers/healthRouter.js
+++ api/routers/healthRouter.js
@@ -1,16 +1,21 @@
 import { Router } from 'express';
 import { healthRepository } from '../data';
 import { asyncMiddleware } from '../middleware';
+import { config } from '../utils/config';
 
 export const healthRouter = Router();
 
 healthRouter.get(
     '/',
     asyncMiddleware(async (_, res) => {
-        const result = await healthRepository.getStatus();
+        const status = await healthRepository.getStatus();
 
         return res.json({
-            data: result
+            data: {
+                version: config.VERSION,
+                stage: config.STAGE,
+                status
+            }
         });
     })
 );
