89f90938afc76210fc0b8e0872c3bd0928c9355b
Chris Poulter <cpoulter@hotmail.co.uk>
2022-01-03 14:33:11 +0000

    clear store on logout

--- src/store.js
+++ src/store.js
@@ -1,15 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { halcyonApi, rtkQueryErrorLogger } from './services';
 import { authReducer, modalReducer, toastReducer } from './features';
 
+const combinedReducer = combineReducers({
+    [halcyonApi.reducerPath]: halcyonApi.reducer,
+    auth: authReducer,
+    modal: modalReducer,
+    toast: toastReducer
+});
+
+const rootReducer = (state, action) => {
+    if (action.type === 'auth/removeToken') {
+        state = undefined;
+    }
+
+    return combinedReducer(state, action);
+};
+
 export const store = configureStore({
-    reducer: {
-        [halcyonApi.reducerPath]: halcyonApi.reducer,
-        auth: authReducer,
-        modal: modalReducer,
-        toast: toastReducer
-    },
+    reducer: rootReducer,
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware({
             serializableCheck: false
