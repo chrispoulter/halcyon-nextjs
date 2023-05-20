add1958570526e0affa11549623dd4db8761a3c9
Chris Poulter <cpoulter@hotmail.co.uk>
2023-05-20 14:58:08 +0100

    code format

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -4,7 +4,8 @@ export const today = endOfToday();
 
 export const currentYear = getYear(today);
 
-export const parseForOutput = (date: string) => parseISO(`${date}T00:00:00.000Z`);
+export const parseForOutput = (date: string) =>
+    parseISO(`${date}T00:00:00.000Z`);
 
 export const formatForInput = (date: string | Date) =>
     format(date instanceof Date ? date : parseISO(date), 'yyyy-MM-dd');
