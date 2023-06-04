e424104f0944572bd74f224e645f3450bb7b5eed
Chris Poulter <cpoulter@hotmail.co.uk>
2023-06-04 11:30:23 +0100

    code clean up

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,34 +1,39 @@
-import { PrismaClient, Users } from '@prisma/client';
+import { Prisma, PrismaClient, Users } from '@prisma/client';
 import { config } from './config';
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
-
-if (config.NODE_ENV === 'development') {
-    global.prisma = prisma;
-}
-
-const computeUserColumns = (user: Users) => {
+const computeUserData = (user: Users) => {
     user.search = `${user.emailAddress} ${user.firstName} ${user.lastName}`;
 };
 
-prisma.$use(async (params, next) => {
+const userMiddleware: Prisma.Middleware = async (params, next) => {
     if (
         params.model == 'Users' &&
         ['create', 'update'].includes(params.action)
     ) {
-        computeUserColumns(params.args.data);
+        computeUserData(params.args.data);
     }
 
     if (params.model == 'Users' && params.action === 'upsert') {
-        computeUserColumns(params.args.create);
-        computeUserColumns(params.args.update);
+        computeUserData(params.args.create);
+        computeUserData(params.args.update);
     }
 
     return next(params);
-});
+};
+
+var prisma = global.prisma;
+
+if (!prisma) {
+    prisma = new PrismaClient();
+    prisma.$use(userMiddleware);
+}
+
+if (config.NODE_ENV === 'development') {
+    global.prisma = prisma;
+}
 
 export default prisma;
