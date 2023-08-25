b9be57694e05d4162bbfa9e415c7469d357ceebf
Chris Poulter <cpoulter@hotmail.co.uk>
2023-08-25 11:53:28 +0100

    Feature/modal refactor (#32)
    * refactor confirmation dialogs
    
    * downgrade nextjs

--- src/features/user/ConfirmLockUser/ConfirmLockUser.tsx
+++ src/features/user/ConfirmLockUser/ConfirmLockUser.tsx
@@ -1,25 +1,49 @@
-import { Confirm, ConfirmProps } from '@/components/Confirm/Confirm';
+import { useState } from 'react';
 import { Modal, ModalBody, ModalTitle } from '@/components/Modal/Modal';
 import { ButtonGroup } from '@/components/ButtonGroup/ButtonGroup';
 import { Button } from '@/components/Button/Button';
 
-type ConfirmLockUserProps = Pick<ConfirmProps, 'children' | 'onConfirm'>;
+type ConfirmLockUserProps = {
+    onConfirm: () => void;
+    loading?: boolean;
+    disabled?: boolean;
+};
 
 export const ConfirmLockUser = ({
-    children,
-    onConfirm
-}: ConfirmLockUserProps) => (
-    <Confirm
-        onConfirm={onConfirm}
-        content={(onOk, onCancel) => (
-            <Modal open={true} onClose={onCancel}>
+    onConfirm,
+    loading,
+    disabled
+}: ConfirmLockUserProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onOk = () => {
+        onClose();
+        onConfirm();
+    };
+
+    const onOpen = () => setIsOpen(true);
+
+    const onClose = () => setIsOpen(false);
+
+    return (
+        <>
+            <Button
+                onClick={onOpen}
+                variant="warning"
+                loading={loading}
+                disabled={disabled}
+            >
+                Lock
+            </Button>
+
+            <Modal open={isOpen} onClose={onClose}>
                 <ModalTitle>Lock User</ModalTitle>
                 <ModalBody>
                     Are you sure you want to lock this user account? The user
                     will no longer be able to access the system.
                 </ModalBody>
                 <ButtonGroup>
-                    <Button variant="secondary" onClick={onCancel}>
+                    <Button variant="secondary" onClick={onClose}>
                         Cancel
                     </Button>
                     <Button variant="warning" onClick={onOk}>
@@ -27,8 +51,6 @@ export const ConfirmLockUser = ({
                     </Button>
                 </ButtonGroup>
             </Modal>
-        )}
-    >
-        {children}
-    </Confirm>
-);
+        </>
+    );
+};
