5821cd5571a43e74f51344437bb044dbca2a5d14
Chris Poulter <cpoulter@hotmail.co.uk>
2023-10-26 16:10:07 +0100

    update seed endpoint

--- src/features/seed/seedTypes.ts
+++
@@ -1,8 +0,0 @@
-export type SeedUser = {
-    emailAddress: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    dateOfBirth: string;
-    roles: string[];
-};
--- src/pages/api/seed/index.ts
+++ src/pages/api/seed/index.ts
@@ -1,35 +1,26 @@
-import { SeedUser } from '@/features/seed/seedTypes';
 import { hashPassword } from '@/utils/hash';
 import { mapHandlers, Handler } from '@/utils/handler';
 import prisma from '@/utils/prisma';
+import { SYSTEM_ADMINISTRATOR } from '@/utils/auth';
 import { config } from '@/utils/config';
 
-const seedUsers: SeedUser[] = [
-    {
+const seedHandler: Handler = async (_, res) => {
+    const user = {
         emailAddress: config.SEED_EMAIL_ADDRESS,
-        password: config.SEED_PASSWORD,
+        password: await hashPassword(config.SEED_PASSWORD),
         firstName: 'System',
         lastName: 'Administrator',
         dateOfBirth: '1970-01-01T00:00:00.000Z',
-        roles: ['SYSTEM_ADMINISTRATOR']
-    }
-];
+        roles: [SYSTEM_ADMINISTRATOR],
+        search: `${config.SEED_EMAIL_ADDRESS} System Administrator`,
+        version: crypto.randomUUID()
+    };
 
-const seedHandler: Handler = async (_, res) => {
-    for (const seedUser of seedUsers) {
-        const user = {
-            ...seedUser,
-            password: await hashPassword(seedUser.password),
-            search: `${seedUser.emailAddress} ${seedUser.firstName} ${seedUser.lastName}`,
-            version: crypto.randomUUID()
-        };
-
-        await prisma.users.upsert({
-            where: { emailAddress: user.emailAddress },
-            update: user,
-            create: user
-        });
-    }
+    await prisma.users.upsert({
+        where: { emailAddress: user.emailAddress },
+        update: user,
+        create: user
+    });
 
     return res.send('Environment seeded.');
 };
