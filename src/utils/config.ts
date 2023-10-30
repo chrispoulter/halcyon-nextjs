f40320dd076773c74fe108d24853292e492866b6
Chris Poulter <cpoulter@hotmail.co.uk>
2023-10-30 15:48:51 +0000

    config clean up

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,9 +2,7 @@ export const config = {
     NODE_ENV: process.env.NODE_ENV,
     VERSION: process.env.NEXT_PUBLIC_GITVERSION_SEMVER || '1.0.0',
 
-    API_URL:
-        process.env.NEXT_PUBLIC_API_URL ||
-        `${process.env.NEXTAUTH_URL || ''}/api`,
+    API_URL: `${process.env.NEXTAUTH_URL || ''}/api`,
 
     DATABASE_URL: process.env.DATABASE_URL!,
 
@@ -15,8 +13,9 @@ export const config = {
     EMAIL_NO_REPLY_ADDRESS:
         process.env.EMAIL_NO_REPLY_ADDRESS || 'noreply@example.com',
 
-    SEED_EMAIL_ADDRESS: process.env.SEED_EMAIL_ADDRESS!,
-    SEED_PASSWORD: process.env.SEED_PASSWORD!,
+    SEED_EMAIL_ADDRESS:
+        process.env.SEED_EMAIL_ADDRESS || 'system.administrator@example.com',
+    SEED_PASSWORD: process.env.SEED_PASSWORD || 'change-me-0987654321',
 
     NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'change-me-1234567890',
