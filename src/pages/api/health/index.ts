3227d932904576254af8dfbc3b859fa965008cfe
Chris Poulter <cpoulter@hotmail.co.uk>
2023-10-31 08:50:19 +0000

    added health endpoint

--- src/pages/api/health/index.ts
+++ src/pages/api/health/index.ts
@@ -1,7 +1,21 @@
 import { NextApiHandler } from 'next';
+import { config } from '@/utils/config';
 
 const healthHandler: NextApiHandler = async (_, res) => {
-    return res.send('Healthy');
+    try {
+        const response = await fetch(`${config.API_URL}/health`, {
+            method: 'GET'
+        });
+
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}.`);
+        }
+
+        return res.send('Healthy');
+    } catch (error) {
+        console.error('api health', error);
+        return res.status(500).send('Unhealthy');
+    }
 };
 
 export default healthHandler;
