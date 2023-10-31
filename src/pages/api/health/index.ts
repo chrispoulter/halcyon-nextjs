e144e6a608f366720a34e7b4c3a7f8832ebec8da
Chris Poulter <cpoulter@hotmail.co.uk>
2023-10-31 09:18:31 +0000

    updated health check

--- src/pages/api/health/index.ts
+++ src/pages/api/health/index.ts
@@ -1,20 +1,26 @@
 import { NextApiHandler } from 'next';
 import { config } from '@/utils/config';
 
+const apiHealthCheck = async () => {
+    const response = await fetch(`${config.API_URL}/health`, {
+        method: 'GET'
+    });
+
+    if (!response.ok) {
+        throw new Error(
+            `Request failed with status ${response.status} ${response.statusText}`
+        );
+    }
+};
+
 const healthHandler: NextApiHandler = async (_, res) => {
     try {
-        const response = await fetch(`${config.API_URL}/health`, {
-            method: 'GET'
-        });
-
-        if (!response.ok) {
-            throw new Error(`Request failed with status ${response.status}.`);
-        }
+        await apiHealthCheck();
 
         return res.send('Healthy');
     } catch (error) {
-        console.error('api health', error);
-        return res.status(500).send('Unhealthy');
+        console.error('Health check failed', error);
+        return res.status(503).send('Unhealthy');
     }
 };
 
