7614a945d7703183d9013df196d23809a40d6129
Chris Poulter <cpoulter@hotmail.co.uk>
2023-10-31 10:30:27 +0000

    use proxy url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
+const proxyApiUrl = process.env.PROXY_API_URL;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone'
+    output: 'standalone',
+    async rewrites() {
+        return [
+            {
+                source: '/api/account/:path*',
+                destination: `${proxyApiUrl}/account/:path*`
+            },
+            {
+                source: '/api/manage/:path*',
+                destination: `${proxyApiUrl}/manage/:path*`
+            },
+            {
+                source: '/api/token/:path*',
+                destination: `${proxyApiUrl}/token/:path*`
+            },
+            {
+                source: '/api/user/:path*',
+                destination: `${proxyApiUrl}/user/:path*`
+            }
+        ];
+    }
 };
 
 module.exports = nextConfig;
