ff8ff3cb8ad752cc74085dcf98e0ea054bbb8162
Chris Poulter <cpoulter@hotmail.co.uk>
2023-11-07 11:35:22 +0000

    update api proxy

--- src/pages/api/[...proxy].tsx
+++ src/pages/api/[...proxy].tsx
@@ -1,11 +1,13 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import { config as utilConfig } from '@/utils/config';
+import { logger } from '@/utils/logger';
 
 const apiProxy = createProxyMiddleware({
     target: utilConfig.EXTERNAL_API_URL,
     secure: false,
     changeOrigin: true,
-    pathRewrite: { '^/api': '' }
+    pathRewrite: { '^/api': '' },
+    logger
 });
 
 export const config = {
