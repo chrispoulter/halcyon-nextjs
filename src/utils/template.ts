0ca99292453c2183945172ed64374c655064b7dc
Chris Poulter <cpoulter@hotmail.co.uk>
2023-11-26 11:16:24 +0000

    manual migrations

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -1,24 +1,14 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
+const folderPath = path.join(process.cwd(), 'src', 'templates');
+
 export const renderTemplate = async (
     template: string,
     data: { [key: string]: any }
 ) => {
-    const resource = await readResource(template);
-    const html = render(resource, data);
-    const subject = getHtmlTitle(resource);
+    const file = await fs.readFile(`${folderPath}/${template}`, 'utf8');
+    const html = file.replace(/{{(.*?)}}/g, (_, key) => data[key] || '');
+    const subject = new RegExp(/<title>(.*?)<\/title>/i).exec(html)![1];
     return [html, subject];
 };
-
-const readResource = (resource: string) =>
-    fs.readFile(
-        `${path.join(process.cwd(), 'src', 'templates')}/${resource}`,
-        'utf8'
-    );
-
-const getHtmlTitle = (template: string) =>
-    new RegExp(/<title>(.*?)<\/title>/i).exec(template)![1];
-
-const render = (html: string, model: { [key: string]: any }) =>
-    html.replace(/{{(.*?)}}/g, (_, key) => model[key] || '');
