1cbdfeb9577f47ba2bacdce58132b345195eab60
Chris Poulter <cpoulter@hotmail.co.uk>
2024-03-17 15:33:45 +0000

    update config

--- src/pages/api/[...proxy].tsx
+++ src/pages/api/[...proxy].tsx
@@ -1,13 +1,11 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import { config as utilConfig } from '@/utils/config';
-import { logger } from '@/utils/logger';
 
 const apiProxy = createProxyMiddleware({
     target: utilConfig.EXTERNAL_API_URL,
     secure: false,
     changeOrigin: true,
-    pathRewrite: { '^/api': '' },
-    logger
+    pathRewrite: { '^/api': '' }
 });
 
 export const config = {
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,18 +1,12 @@
 export const config = {
-    NODE_ENV: process.env.NODE_ENV,
     VERSION: process.env.NEXT_PUBLIC_VERSION || '1.0.0',
-    LOG_LEVEL: process.env.LOG_LEVEL || 'info',
-
     API_URL: `${process.env.NEXTAUTH_URL || ''}/api`,
-
     EXTERNAL_API_URL: process.env.EXTERNAL_API_URL || 'https://localhost:7257',
-
     JWT_SECURITY_KEY:
         process.env.JWT_SECURITY_KEY ||
         'X46npCfZgsMFwJNnLk3Yb9yUPW9x6SuD8ChZ6cVooSZMdtHHPEkwPkQoHUsNDLTanSEiG7y6shTXHePUZ5fr6i5J3R2cMvCRNGeq55jRXRxAeeSwA46ro5bTVSBiif86',
     JWT_ISSUER: process.env.JWT_ISSUER || 'HalcyonApi',
     JWT_AUDIENCE: process.env.JWT_AUDIENCE || 'HalcyonClient',
-
     NEXTAUTH_SECRET:
         process.env.NEXTAUTH_SECRET ||
         'YenEZqADi2YnfVCNWMFkcUpmr55ESFepmHgH3NSmPoBeSqf9Hs3Ui8vb6xdkCY9PNewqNxVQdjkqhCGaWeyri48FyDPU9HfKFjbHcfbyq4Fuqmk2suehduYJYzuPoSD8',
