0fad7dea1294ff9699441f390b216dc84f4f92c6
Chris Poulter <cpoulter@hotmail.co.uk>
2024-05-02 16:39:00 +0100

    update test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: process.env.CI ? 'github' : 'html',
     use: {
-        baseURL: 'http://127.0.0.1:3000',
+        baseURL: 'http://localhost:3000',
         trace: 'on-first-retry',
         screenshot: 'only-on-failure'
     },
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
     webServer: {
         command: 'npm run dev',
-        url: 'http://127.0.0.1:3000',
+        url: 'http://localhost:3000',
         reuseExistingServer: !process.env.CI
     }
 });
