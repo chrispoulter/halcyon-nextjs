e61b9783e6e9f4af2177652ee77b1f9a817d428e
Chris Poulter <cpoulter@hotmail.co.uk>
2024-05-22 11:47:10 +0100

    re use existing server

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,6 @@ export default defineConfig({
     webServer: {
         command: 'npm run dev',
         url: baseURL,
-        reuseExistingServer: !process.env.CI
+        reuseExistingServer: true
     }
 });
