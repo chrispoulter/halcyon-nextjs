ff0b557985ae607311b77e6045898e37c3d28688
Chris Poulter <cpoulter@hotmail.co.uk>
2024-06-16 11:00:06 +0100

    code clean up

--- src/features/manage/manageEndpoints.ts
+++ src/features/manage/manageEndpoints.ts
@@ -7,53 +7,45 @@ import {
     DeleteAccountRequst
 } from './manageTypes';
 
-export const manageEndpoints = api
-    .enhanceEndpoints({ addTagTypes: ['User'] })
-    .injectEndpoints({
-        endpoints: builder => ({
-            getProfile: builder.query<GetProfileResponse, void>({
-                query: () => '/manage',
-                providesTags: result => [{ type: 'User', id: result?.id }]
-            }),
-            updateProfile: builder.mutation<
-                UpdatedResponse,
-                UpdateProfileRequest
-            >({
-                query: body => ({
-                    url: '/manage',
-                    method: 'PUT',
-                    body
-                }),
-                invalidatesTags: (result, error) =>
-                    error ? [] : [{ type: 'User', id: result?.id }]
-            }),
-            changePassword: builder.mutation<
-                UpdatedResponse,
-                ChangePasswordRequest
-            >({
-                query: body => ({
-                    url: '/manage/change-password',
-                    method: 'PUT',
-                    body
-                }),
-                invalidatesTags: (result, error) =>
-                    error ? [] : [{ type: 'User', id: result?.id }]
-            }),
-            deleteAccount: builder.mutation<
-                UpdatedResponse,
-                DeleteAccountRequst
-            >({
-                query: body => ({
-                    url: '/manage',
-                    method: 'DELETE',
-                    body
-                }),
-                invalidatesTags: (_, error) =>
-                    error ? [] : [{ type: 'User', id: 'PARTIAL-LIST' }]
-            })
+export const manageEndpoints = api.injectEndpoints({
+    endpoints: builder => ({
+        getProfile: builder.query<GetProfileResponse, void>({
+            query: () => '/manage',
+            providesTags: result => [{ type: 'User', id: result?.id }]
         }),
-        overrideExisting: false
-    });
+        updateProfile: builder.mutation<UpdatedResponse, UpdateProfileRequest>({
+            query: body => ({
+                url: '/manage',
+                method: 'PUT',
+                body
+            }),
+            invalidatesTags: (result, error) =>
+                error ? [] : [{ type: 'User', id: result?.id }]
+        }),
+        changePassword: builder.mutation<
+            UpdatedResponse,
+            ChangePasswordRequest
+        >({
+            query: body => ({
+                url: '/manage/change-password',
+                method: 'PUT',
+                body
+            }),
+            invalidatesTags: (result, error) =>
+                error ? [] : [{ type: 'User', id: result?.id }]
+        }),
+        deleteAccount: builder.mutation<UpdatedResponse, DeleteAccountRequst>({
+            query: body => ({
+                url: '/manage',
+                method: 'DELETE',
+                body
+            }),
+            invalidatesTags: (_, error) =>
+                error ? [] : [{ type: 'User', id: 'PARTIAL-LIST' }]
+        })
+    }),
+    overrideExisting: false
+});
 
 export const {
     useGetProfileQuery,
