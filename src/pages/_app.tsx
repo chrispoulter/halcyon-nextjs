5575f7d6c54cf4bb2d9fa8564b1ebb406013012a
Chris Poulter <cpoulter@hotmail.co.uk>
2024-08-21 12:30:29 +0100

    remove messages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { Open_Sans } from 'next/font/google';
 import router from 'next/router';
@@ -25,115 +24,94 @@ const font = Open_Sans({
     display: 'swap'
 });
 
+const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+        onError: async (error: any) => {
+            switch (error.status) {
+                case 401:
+                    await signOut({ callbackUrl: '/' });
+                    break;
+
+                case 403:
+                    router.push('/403', router.asPath);
+                    break;
+
+                case 404:
+                    router.push('/404', router.asPath);
+                    break;
+
+                default:
+                    router.push('/500', router.asPath);
+                    break;
+            }
+        }
+    }),
+    mutationCache: new MutationCache({
+        onSuccess: (data: any) => {
+            if (data.message) {
+                toast.success(data.message);
+            }
+        },
+        onError: async (error: any) => {
+            const message = error.response?.message || error.message;
+
+            switch (error.status) {
+                case 401:
+                    await signOut({ callbackUrl: '/' });
+                    break;
+
+                case 403:
+                    toast.error(
+                        'Sorry, you do not have access to this resource.'
+                    );
+                    break;
+
+                case 404:
+                    toast.error(
+                        'Sorry, the resource you were looking for could not be found.'
+                    );
+                    break;
+
+                default:
+                    toast.error(
+                        message ||
+                            'Sorry, something went wrong. Please try again later.'
+                    );
+                    break;
+            }
+        }
+    })
+});
+
 const App = ({
     Component,
     pageProps: { session, dehydratedState = {}, ...pageProps }
-}: AppProps) => {
-    const [queryClient] = useState(
-        () =>
-            new QueryClient({
-                // defaultOptions: {
-                //     queries: {
-                //         refetchOnWindowFocus: false,
-                //         refetchOnMount: false,
-                //         refetchOnReconnect: false,
-                //         retry: false,
-                //         staleTime: Infinity,
-                //         cacheTime: Infinity
-                //     },
-                //     mutations: {
-                //         retry: false,
-                //         cacheTime: Infinity
-                //     }
-                // },
-                queryCache: new QueryCache({
-                    onError: async (error: any) => {
-                        switch (error.status) {
-                            case 401:
-                                await signOut({ callbackUrl: '/' });
-                                break;
+}: AppProps) => (
+    <>
+        <Meta />
 
-                            case 403:
-                                router.push('/403', router.asPath);
-                                break;
+        <style jsx global>{`
+            :root {
+                --base-font: ${font.style.fontFamily};
+            }
+        `}</style>
 
-                            case 404:
-                                router.push('/404', router.asPath);
-                                break;
-
-                            default:
-                                router.push('/500', router.asPath);
-                                break;
-                        }
-                    }
-                }),
-                mutationCache: new MutationCache({
-                    onSuccess: (data: any) => {
-                        if (data.message) {
-                            toast.success(data.message);
-                        }
-                    },
-                    onError: async (error: any) => {
-                        const message =
-                            error.response?.message || error.message;
-
-                        switch (error.status) {
-                            case 401:
-                                await signOut({ callbackUrl: '/' });
-                                break;
-
-                            case 403:
-                                toast.error(
-                                    'Sorry, you do not have access to this resource.'
-                                );
-                                break;
-
-                            case 404:
-                                toast.error(
-                                    'Sorry, the resource you were looking for could not be found.'
-                                );
-                                break;
-
-                            default:
-                                toast.error(
-                                    message ||
-                                        'Sorry, something went wrong. Please try again later.'
-                                );
-                                break;
-                        }
-                    }
-                })
-            })
-    );
-
-    return (
-        <>
-            <Meta />
-
-            <style jsx global>{`
-                :root {
-                    --base-font: ${font.style.fontFamily};
-                }
-            `}</style>
-
-            <SessionProvider session={session}>
-                <QueryClientProvider client={queryClient}>
-                    <HydrationBoundary state={dehydratedState}>
-                        <Header />
-                        <main>
-                            <ErrorBoundary>
-                                <Component {...pageProps} />
-                            </ErrorBoundary>
-                        </main>
-                        <Footer />
-                        {/* <Messages /> */}
-                    </HydrationBoundary>
-                    <ReactQueryDevtools initialIsOpen={false} />
-                </QueryClientProvider>
-            </SessionProvider>
-            <Toaster />
-        </>
-    );
-};
+        <SessionProvider session={session}>
+            <QueryClientProvider client={queryClient}>
+                <HydrationBoundary state={dehydratedState}>
+                    <Header />
+                    <main>
+                        <ErrorBoundary>
+                            <Component {...pageProps} />
+                        </ErrorBoundary>
+                    </main>
+                    <Footer />
+                </HydrationBoundary>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </SessionProvider>
+        <Toaster />
+    </>
+);
 
 export default App;
