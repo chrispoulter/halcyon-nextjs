f8ddefbef8ec2ba66124d7e78ad0aeb0259e71a8
Chris Poulter <cpoulter@hotmail.co.uk>
2024-08-23 11:21:56 +0100

    update component props

--- src/features/user/UpdateUserForm/UpdateUserForm.tsx
+++ src/features/user/UpdateUserForm/UpdateUserForm.tsx
@@ -128,22 +128,10 @@ const UpdateUserFormInternal = ({
     </Formik>
 );
 
-export const UpdateUserForm = ({
-    user,
-    isDisabled,
-    onSubmit,
-    options
-}: UpdateUserFormProps) => {
+export const UpdateUserForm = ({ user, ...props }: UpdateUserFormProps) => {
     if (!user) {
         return <UpdateUserFormLoading />;
     }
 
-    return (
-        <UpdateUserFormInternal
-            user={user}
-            isDisabled={isDisabled}
-            onSubmit={onSubmit}
-            options={options}
-        />
-    );
+    return <UpdateUserFormInternal {...props} user={user} />;
 };
