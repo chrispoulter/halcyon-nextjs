ea024bba9fa57dcdb3c5eb8cd5030a7fff2c0a9d
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-01 14:37:45 +0100

    bugfix typo

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -20,7 +20,7 @@ export const Meta = ({
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
 
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
         <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
