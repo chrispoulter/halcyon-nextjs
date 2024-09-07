4d40e957edca14209b4b9cb57c0cbcaf5f1f20c0
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-07 16:10:56 +0100

    update next.config.mjs to include serverRuntimeConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone'
+    output: 'standalone',
+    serverRuntimeConfig: {
+        host: '0.0.0.0',
+        port: 3000
+    }
 };
 
 export default nextConfig;
