59edac906052b68e6ef385bde47dcf0c644f2fc1
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-08 08:13:03 +0100

    update config variable for docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
-    serverRuntimeConfig: {
-        host: '0.0.0.0',
-        port: 3000
-    }
+    output: 'standalone'
 };
 
 export default nextConfig;
