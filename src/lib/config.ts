f042c467b42b99b9018575c85f62a2ea2422a405
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-09 16:27:14 +0100

    rollback config changes

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,15 @@
 export const config = {
-    VERSION: process.env.NEXT_PUBLIC_VERSION!,
+    VERSION: process.env.NEXT_PUBLIC_VERSION || '1.0.0-local',
     API_URL:
-        process.env.NEXT_SERVER_API_URL || process.env.NEXT_PUBLIC_API_URL!,
-    JWT_SECURITY_KEY: process.env.JWT_SECURITY_KEY!,
-    JWT_ISSUER: process.env.JWT_ISSUER!,
-    JWT_AUDIENCE: process.env.JWT_AUDIENCE!
+        process.env.NEXT_SERVER_API_URL ||
+        process.env.NEXT_PUBLIC_API_URL ||
+        'http://localhost:5257',
+    JWT_SECURITY_KEY:
+        process.env.JWT_SECURITY_KEY ||
+        'X46npCfZgsMFwJNnLk3Yb9yUPW9x6SuD8ChZ6cVooSZMdtHHPEkwPkQoHUsNDLTanSEiG7y6shTXHePUZ5fr6i5J3R2cMvCRNGeq55jRXRxAeeSwA46ro5bTVSBiif86',
+    JWT_ISSUER: process.env.JWT_ISSUER || 'HalcyonApi',
+    JWT_AUDIENCE: process.env.JWT_AUDIENCE || 'HalcyonClient',
+    NEXTAUTH_SECRET:
+        process.env.NEXTAUTH_SECRET ||
+        'YenEZqADi2YnfVCNWMFkcUpmr55ESFepmHgH3NSmPoBeSqf9Hs3Ui8vb6xdkCY9PNewqNxVQdjkqhCGaWeyri48FyDPU9HfKFjbHcfbyq4Fuqmk2suehduYJYzuPoSD8',
 };
