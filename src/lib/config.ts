e23863de1ac8b999766fd84e37a614d22f5a98a2
Chris Poulter <cpoulter@hotmail.co.uk>
2024-09-09 12:25:27 +0100

    update default config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -8,11 +8,5 @@ export const config = {
         process.env.JWT_SECURITY_KEY ||
         'X46npCfZgsMFwJNnLk3Yb9yUPW9x6SuD8ChZ6cVooSZMdtHHPEkwPkQoHUsNDLTanSEiG7y6shTXHePUZ5fr6i5J3R2cMvCRNGeq55jRXRxAeeSwA46ro5bTVSBiif86',
     JWT_ISSUER: process.env.JWT_ISSUER || 'HalcyonApi',
-    JWT_AUDIENCE: process.env.JWT_AUDIENCE || 'HalcyonClient',
-    NEXTAUTH_SECRET:
-        process.env.NEXTAUTH_SECRET ||
-        'YenEZqADi2YnfVCNWMFkcUpmr55ESFepmHgH3NSmPoBeSqf9Hs3Ui8vb6xdkCY9PNewqNxVQdjkqhCGaWeyri48FyDPU9HfKFjbHcfbyq4Fuqmk2suehduYJYzuPoSD8',
-    NEXTAUTH_SESSION_MAXAGE: parseInt(
-        process.env.NEXTAUTH_SESSION_MAXAGE || '3600'
-    )
+    JWT_AUDIENCE: process.env.JWT_AUDIENCE || 'HalcyonClient'
 };
