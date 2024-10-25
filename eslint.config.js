8f44b32674e41060b142ca928d0d638fa6457c82
Chris Poulter <cpoulter@hotmail.co.uk>
2024-10-25 09:27:03 +0100

    upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import vitest from 'eslint-plugin-vitest';
+import playwright from 'eslint-plugin-playwright';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+const eslintConfig = [
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    eslintPluginPrettierRecommended,
+    {
+        files: ['src/tests/**'],
+        plugins: {
+            vitest
+        },
+        rules: {
+            ...vitest.configs.recommended.rules
+        }
+    },
+    {
+        ...playwright.configs['flat/recommended'],
+        files: ['e2e/**'],
+        rules: {
+            ...playwright.configs['flat/recommended'].rules
+        }
+    },
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 0
+        }
+    }
+];
+
+export default eslintConfig;
