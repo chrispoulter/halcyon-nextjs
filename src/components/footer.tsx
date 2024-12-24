01bda76c64b180003e9391104f0c186ec1f9d85a
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-24 14:49:23 +0000

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,21 @@
+import { currentYear } from '@/lib/dates';
+
 export function Footer() {
     return (
-        <footer className="mx-auto flex max-w-screen-sm justify-between gap-6 p-6">
-            <div className="leading-7">
-                &copy;{' '}
-                <a
-                    href="http://www.chrispoulter.com"
-                    className="font-medium text-primary underline underline-offset-4"
-                >
-                    Chris Poulter
-                </a>{' '}
-                2024
+        <footer className="mt-6 border-t">
+            <div className="mx-auto flex max-w-screen-sm flex-col justify-between gap-2 p-6 text-center sm:flex-row sm:text-left">
+                <div className="text-sm font-medium leading-none">
+                    &copy;{' '}
+                    <a
+                        href="http://www.chrispoulter.com"
+                        className="font-medium text-primary underline underline-offset-4"
+                    >
+                        Chris Poulter
+                    </a>{' '}
+                    {currentYear}
+                </div>
+                <div className="text-sm font-medium leading-none">v1.0.0</div>
             </div>
-            <div className="leading-7">v1.0.0</div>
         </footer>
     );
 }
