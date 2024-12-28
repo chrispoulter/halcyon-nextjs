6a00428a56961b776b5fdba55b93466e61afb3da
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-28 15:15:50 +0000

    update open telemetry

--- src/instrumentation.node.ts
+++
@@ -1,4 +0,0 @@
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
-
-export const spanProcessors = [new BatchSpanProcessor(new OTLPTraceExporter())];
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,5 @@
 import { registerOTel } from '@vercel/otel';
 
-export async function register() {
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-        const { spanProcessors } = await import('./instrumentation.node');
-        registerOTel({ spanProcessors });
-    }
+export function register() {
+    registerOTel({ serviceName: 'halcyon' });
 }
