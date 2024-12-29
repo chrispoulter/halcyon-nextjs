0fa47e72902ccb681013cd926346dba083923bdd
Chris Poulter <cpoulter@hotmail.co.uk>
2024-12-29 10:49:18 +0000

    update user card

--- src/app/user/user-card.tsx
+++ src/app/user/user-card.tsx
@@ -10,15 +10,16 @@ type UserCardProps = {
 export function UserCard({ user }: UserCardProps) {
     return (
         <Link
-            key={user.id}
             href={`/user/${user.id}`}
-            className="block space-y-2 rounded-lg border p-3 transition-all focus-within:bg-accent hover:bg-accent"
+            className="block space-y-2 rounded-lg border p-4 focus-within:bg-accent hover:bg-accent"
         >
-            <div className="truncate text-sm font-medium leading-tight">
-                {user.firstName} {user.lastName}
-            </div>
-            <div className="truncate text-sm text-muted-foreground">
-                {user.emailAddress}
+            <div className="space-y-0.5">
+                <div className="truncate text-base font-medium">
+                    {user.firstName} {user.lastName}
+                </div>
+                <div className="truncate text-sm text-muted-foreground">
+                    {user.emailAddress}
+                </div>
             </div>
             <div className="flex flex-col gap-2 sm:flex-row">
                 {user.isLockedOut && (
