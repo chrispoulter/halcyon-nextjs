073c25b0b14bc3b4fbcfe2c77b6df30541bac236
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-20 09:33:45 +0000

    try again with version number

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'standalone',
+    env: {
+        npm_package_name: process.env.npm_package_name,
+        npm_package_version: process.env.npm_package_version,
+    },
     experimental: {
         authInterrupts: true,
     },
