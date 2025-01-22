53e48e67e0a539914e788df9e8eccba5af1cc1a3
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-22 14:04:47 +0000

    update config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     output: 'standalone',
     env: {
-        npm_package_name: process.env.npm_package_name,
         npm_package_version: process.env.npm_package_version,
     },
     experimental: {
