8e8030d6855170b5e23eb84f0201a766fa80e458
Chris Poulter <cpoulter@hotmail.co.uk>
2025-01-24 16:28:37 +0000

    add more loading screens

---
+++ src/app/account/login/loading.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function LoginLoading() {
+    return (
+        <main className="mx-auto max-w-screen-sm space-y-6 p-6">
+            <Skeleton className="h-14 w-3/4" />
+            <Skeleton className="h-14" />
+
+            <div className="space-y-6">
+                <Skeleton className="h-14" />
+                <Skeleton className="h-14" />
+
+                <div className="flex flex-col justify-end gap-2 sm:flex-row">
+                    <Skeleton className="h-10 w-full sm:w-32" />
+                </div>
+            </div>
+
+            <div className="space-y-2">
+                <Skeleton className="h-5 w-64" />
+                <Skeleton className="h-5 w-64" />
+            </div>
+        </main>
+    );
+}
