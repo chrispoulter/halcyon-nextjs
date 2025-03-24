670a015cfe7ef6e21a294c06bea7c36e6a79b3a0
Chris Poulter <cpoulter@hotmail.co.uk>
2025-03-24 12:52:34 +0000

    add email service

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,3 +1,9 @@
+import path from 'path';
+import { SMTPClient } from 'emailjs';
+import { promises as fs } from 'fs';
+import { config } from '@/lib/config';
+import { getSiteUrl } from '@/lib/server-utils';
+
 export enum EmailTemplate {
     ResetPassword = 'RESET_PASSWORD',
 }
@@ -9,7 +15,56 @@ type EmailMessage = {
     context: object;
 };
 
-export const sendEmail = async (message: EmailMessage): Promise<void> => {
-    // TODO: Implement sendEmail
-    console.log('Sending email:', message);
+export const sendEmail = async (message: EmailMessage) => {
+    const template = await readResource(message.template);
+    const title = getTitle(template);
+
+    const cdnUrl = await getSiteUrl();
+    const data = { ...message.context, cdnUrl };
+
+    const subject = replaceData(title, data);
+    const body = replaceData(template, data);
+
+    const client = new SMTPClient({
+        host: config.EMAIL_SMTP_SERVER,
+        port: config.EMAIL_SMTP_PORT,
+        user: config.EMAIL_SMTP_USERNAME,
+        password: config.EMAIL_SMTP_PASSWORD,
+    });
+
+    try {
+        await client.sendAsync({
+            from: message.from || config.EMAIL_NO_REPLY_ADDRESS,
+            to: message.to,
+            subject,
+            text: body,
+            attachment: [
+                {
+                    data: body,
+                    alternative: true,
+                },
+            ],
+        });
+    } catch (error) {
+        console.error('email error', error);
+    }
+};
+
+const readResource = (resource: string) =>
+    fs.readFile(
+        `${path.join(process.cwd(), 'src', 'templates')}/${resource}.html`,
+        'utf8'
+    );
+
+const getTitle = (template: string) =>
+    new RegExp(/<title>\s*(.+?)\s*<\/title>/).exec(template)![1];
+
+const replaceData = (str: string, obj: object) => {
+    let result = str;
+
+    for (const [key, replaceValue] of Object.entries(obj)) {
+        result = result.replace(new RegExp(`{{ ${key} }}`, 'g'), replaceValue);
+    }
+
+    return result;
 };
