aa2967288d71a2572413d2a4a0a03c1cb7bfff4f
Chris Poulter <cpoulter@hotmail.co.uk>
2025-03-28 13:25:21 +0000

    update read resource

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -15,6 +15,8 @@ type EmailMessage = {
     context: object;
 };
 
+const TEMPLATES_PATH = 'public/templates';
+
 export const sendEmail = async (message: EmailMessage) => {
     const template = await readResource(message.template);
     const title = getTitle(template);
@@ -52,10 +54,7 @@ export const sendEmail = async (message: EmailMessage) => {
 };
 
 const readResource = (resource: string) =>
-    fs.readFile(
-        path.join(process.cwd(), 'public', 'templates', `${resource}.html`),
-        'utf8'
-    );
+    fs.readFile(path.resolve(`${TEMPLATES_PATH}/${resource}.html`), 'utf8');
 
 const getTitle = (template: string) =>
     new RegExp(/<title>\s*(.+?)\s*<\/title>/).exec(template)![1];
