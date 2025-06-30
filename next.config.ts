0aebc4f6caa9e3e402ec606793d123411e7f3506
Chris Poulter <cpoulter@hotmail.co.uk>
2025-06-30 16:22:27 +0100

    update version env

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     output: 'standalone',
     env: {
-        npm_package_version: process.env.npm_package_version,
+        VERSION:
+            process.env.VERCEL_DEPLOYMENT_ID || process.env.npm_package_version,
     },
     experimental: {
         authInterrupts: true,
