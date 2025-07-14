ada8efff39c701b4fef974fc061a2f588348400c
Chris Poulter <cpoulter@hotmail.co.uk>
2025-07-14 09:52:22 +0100

    add db seeder

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,9 +2,7 @@ import { registerOTel } from '@vercel/otel';
 
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
-        await import('./db/create');
-        await import('./db/migrate');
-        await import('./db/seed');
+        await import('./db/seeder');
     }
 
     registerOTel({ serviceName: 'halcyon-nextjs' });
