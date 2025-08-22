a1cdc9d9bb1a130f1bf9e68d8f637524a912edc2
Chris Poulter <cpoulter@hotmail.co.uk>
2025-08-22 11:22:02 +0100

    update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
     {
-        rules: {
-            '@typescript-eslint/no-explicit-any': 0,
-        },
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'out/**',
+            'build/**',
+            'next-env.d.ts',
+        ],
     },
 ];
 
