1d55b060293da90bd7698f257c92aeb0de5e7ee0
Chris Poulter <cpoulter@hotmail.co.uk>
2025-08-22 11:32:55 +0100

    add route props helpers

--- src/app/account/reset-password/[token]/page.tsx
+++ src/app/account/reset-password/[token]/page.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from 'next';
 import { ResetPassword } from '@/app/account/reset-password/[token]/reset-password';
 
-type ResetPasswordPageProps = {
-    params: Promise<{ token: string }>;
-};
-
 export const metadata: Metadata = {
     title: 'Reset Password',
 };
 
 export default async function ResetPasswordPage({
     params,
-}: ResetPasswordPageProps) {
+}: PageProps<'/account/reset-password/[token]'>) {
     const { token } = await params;
 
     return <ResetPassword token={token} />;
