8e91bf6854b9c6de9f1a63bf1f0b47930699cda8
Chris Poulter <cpoulter@hotmail.co.uk>
2025-12-20 09:09:40 +0000

    update token implementation

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,9 +1,11 @@
-import crypto from 'crypto';
+import { createHash, timingSafeEqual } from 'crypto';
 
 export function hashToken(token: string) {
-    return crypto.createHash('sha512').update(token).digest('hex');
+    return createHash('sha512').update(token).digest('hex');
 }
 
 export function verifyToken(token: string, hashedToken: string) {
-    return hashedToken === hashToken(token);
+    const actual = createHash('sha512').update(token).digest();
+    const expected = Buffer.from(hashedToken, 'hex');
+    return timingSafeEqual(actual, expected);
 }
