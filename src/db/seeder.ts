b4ddd1136369032011efe28f9bc5b1d61c605909
Chris Poulter <cpoulter@hotmail.co.uk>
2026-05-29 12:01:40 +0100

    refactor: add POSTGRES_DB environment variable to docker-compose and remove createDb function from seeder

--- src/db/seeder.ts
+++ src/db/seeder.ts
@@ -1,48 +1,10 @@
 import 'dotenv/config';
 import path from 'node:path';
-import { Client } from 'pg';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { generateHash } from '@/lib/hash';
 import { config } from '@/lib/config';
 import { users } from './schema/users';
 import { db } from '.';
-
-async function createDb() {
-    console.log('Creating database...');
-
-    const mainClient = new Client(config.DATABASE_URL);
-
-    const { host, port, ssl, user, password, database } = mainClient;
-
-    const postgresClient = new Client({
-        host,
-        port,
-        ssl,
-        user,
-        password,
-        database: 'postgres',
-    });
-
-    try {
-        await postgresClient.connect();
-
-        const { rows } = await postgresClient.query(
-            'SELECT 1 FROM pg_database WHERE datname = $1',
-            [database]
-        );
-
-        if (rows.length) {
-            console.log(`Database "${database}" already exists.`);
-            return;
-        }
-
-        await postgresClient.query(`CREATE DATABASE "${database}"`);
-    } catch (error) {
-        console.error('Failed to create database', error);
-    } finally {
-        await postgresClient.end();
-    }
-}
 
 async function migrateDb() {
     console.log('Migrating database...');
@@ -84,7 +46,6 @@ async function seedDb() {
 }
 
 async function main() {
-    await createDb();
     await migrateDb();
     await seedDb();
 }
