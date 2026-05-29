a53fd4e8615f1a38f496acee4d6793cf69f58546
Chris Poulter <cpoulter@hotmail.co.uk>
2026-05-29 16:40:05 +0100

    refactor: remove conditional database seeder import from instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,9 +1,5 @@
 import { registerOTel } from '@vercel/otel';
 
 export async function register() {
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-        await import('./db/seeder');
-    }
-
     registerOTel({ serviceName: 'halcyon-nextjs' });
 }
