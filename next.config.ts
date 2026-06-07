8f81390685c4415a252b7590fa8ef1cca148f0ee
Chris Poulter <cpoulter@hotmail.co.uk>
2026-06-07 08:10:45 +0100

    refactor: update APP_VERSION to use git commit SHA or npm package version in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from 'next';
 
+const gitCommitSha =
+    process.env.VITE_VERCEL_GIT_COMMIT_SHA ||
+    process.env.GIT_COMMIT_SHA ||
+    undefined;
+
+const version = gitCommitSha?.slice(0, 7) ?? process.env.npm_package_version;
+
 const nextConfig: NextConfig = {
     output: 'standalone',
     outputFileTracingIncludes: {
         '/*': ['./drizzle/**'],
     },
     env: {
-        APP_VERSION: process.env.npm_package_version,
+        APP_VERSION: version,
     },
     experimental: {
         authInterrupts: true,
